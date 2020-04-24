910a6921b4236f1a6e6d1fa5afe0f08eed508bda
Shivraj97 <43899907+Shivraj97@users.noreply.github.com>
2020-04-24 13:12:05 +0530

    updated

--- dist/redditSearchApp.e31bb0bc.js
+++ dist/redditSearchApp.e31bb0bc.js
@@ -231,7 +231,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50749" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53875" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
